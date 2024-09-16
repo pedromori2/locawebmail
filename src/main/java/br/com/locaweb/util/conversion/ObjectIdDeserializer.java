3b4d863e152d9e1c52df9fed534cdfbb9e22d240
comar80 <comar80@gmail.com>
2024-09-16 19:41:06 -0300

    conversao json para userid

---
+++ src/main/java/br/com/locaweb/util/conversion/ObjectIdDeserializer.java
@@ -0,0 +1,18 @@
+package br.com.locaweb.util.conversion;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+
+public class ObjectIdDeserializer extends JsonDeserializer<ObjectId> {
+
+    @Override
+    public ObjectId deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        String objectIdString = jsonParser.getText();
+        return new ObjectId(objectIdString);  // Convert String back to ObjectId
+    }
+}
+
