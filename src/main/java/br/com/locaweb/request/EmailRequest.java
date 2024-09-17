649119644509d94385d9576bd300d2c6c496ec92
pedromori <pedrohiroshigue@gmail.com>
2024-09-17 17:59:23 -0300

    Ajuste EmailRequest

--- src/main/java/br/com/locaweb/request/EmailRequest.java
+++ src/main/java/br/com/locaweb/request/EmailRequest.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.Date;
 import java.util.List;
@@ -14,21 +15,32 @@ import java.util.List;
 @Data
 public class EmailRequest {
 
-    @NotBlank(message = "'caixaEmail_id' must be a string and is required")
+    @NotBlank
+    @Field(name = "caixaEmail_id")
     private CaixaEmailEnum caixaEmailId;
 
-    @NotNull(message = "'user_id' must be an objectId and is required")
-    private ObjectId userId;  // Considerando que objectId será tratado como String ou um tipo adequado
+    @NotNull
+    @Field(name = "user_id")
+    private ObjectId userId;
 
-    @NotBlank(message = "'email_de' must be a string and is required")
+    @NotBlank
+    @Field(name = "email_de")
     private String emailDe;
 
-    @NotEmpty(message = "'email_para' must be an array of strings and is required")
-    private List<@Email(message = "'email_para' must contain valid email addresses") String> emailPara;
+    @NotEmpty
+    @Email
+    @Field(name = "email_para")
+    private List<String> emailPara;
 
-    private List<@Email(message = "'email_cc' must contain valid email addresses") String> emailCc;
+    @NotEmpty
+    @Email
+    @Field(name = "email_cc")
+    private List<String> emailCc;
 
-    private List<@Email(message = "'email_cco' must contain valid email addresses") String> emailCco;
+    @Email
+    @NotEmpty
+    @Field(name = "email_cco")
+    private List<String> emailCco;
 
     @NotNull(message = "'horario' must be a date and is required")
     private Date horario;
